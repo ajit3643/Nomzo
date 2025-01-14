c70a6f25bbe9358d7d1693fd9e1c8b0bb36311b0
Kumar Ajit <ajitinfotech28@gmail.com>
2025-01-15 00:21:17 +0530

    Top rated restaurant filtered

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,18 +1,28 @@
 import Restaurant from "./Restaurant";
+import { useState } from "react";
 import "./style.css";
-
 import resList from "../../utils/mockData";
-
 const Body = () => {
+  const [listOfRestaurant, setResList] = useState(resList);
   return (
     <div className="body">
       <div className="search-container">
         <input type="text" placeholder="Search for restaurant" />
-        <button>Search</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterdRestList = listOfRestaurant.filter((res) => {
+              return res.info.avgRating > 4;
+            });
+            setResList(filterdRestList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
 
       <div className="restaurant-container">
-        {resList.map((restaurant) => {
+        {listOfRestaurant.map((restaurant) => {
           return <Restaurant key={restaurant.info.id} resData={restaurant} />;
         })}
       </div>
