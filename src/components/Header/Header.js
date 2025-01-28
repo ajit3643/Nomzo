7b989e6919f0124b01f658733b9e47dec1f7864b
Kumar Ajit <ajitinfotech28@gmail.com>
2025-01-28 18:42:59 +0530

    Add material-ui in nomzo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
 import "./style.css";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import Badge, { badgeClasses } from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCartOutlined";
 import LOGO from "../../assets/Nomzo.png";
-import CART from "../../assets/carts.png";
 import { useState } from "react";
+
+const CartBadge = styled(Badge)`
+  & .${badgeClasses.badge} {
+    top: -12px;
+    right: -6px;
+  }
+`;
+
 // Header Component
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -25,19 +37,22 @@ const Header = () => {
           <li>
             <a href="#contact">Contact</a>
           </li>
-          <button
-            className="button"
+          <Button
+            variant="contained"
+            color="success"
             onClick={() => {
               btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
             }}
           >
             {btnName}
-          </button>
+          </Button>
         </ul>
       </div>
       <div className="cart">
-        <img src={CART} alt="cart-icon" />
-        <span>3</span>
+        <IconButton>
+          <ShoppingCartIcon fontSize="small" />
+          <CartBadge badgeContent={2} color="primary" overlap="circular" />
+        </IconButton>
       </div>
     </div>
   );
